a1fca45f228fddf690da656f8ddfe544e68bf03e
Alexander Machic <alexandevcwa@gmail.com>
2026-09-07 00:32:54 -0600

    refactor: update validation patterns and constraints for CUI, NIT, pasaporte, genero, telefono, correo, direccion, and locacionId in RegistrarVecinoCmd

--- asiic-backend/src/main/java/com/assic/muni/application/cqrs/cmd/RegistrarVecinoCmd.java
+++ asiic-backend/src/main/java/com/assic/muni/application/cqrs/cmd/RegistrarVecinoCmd.java
@@ -8,18 +8,19 @@ import lombok.Data;
 public class RegistrarVecinoCmd {
 
     @NotBlank
-    @Pattern(regexp = "\\d{13}", message = "El CUI debe contener 13 dígitos")
+    @Pattern(regexp = "\\d{13}", message = "Formato de CUI es invalido")
     @JsonProperty("cui")
     private String cui;
 
-    @Size(max = 12)
-    @Pattern(regexp = "\\d{0,12}", message = "NIT inválido")
+    @Size(max = 13)
+    @Pattern(regexp = "\\d*[a-zA-Z]?$", message = "Formato de NIT es invalido")
     @JsonProperty("nit")
-    private String nit;              // RN2: Opcional
+    private String nit;
 
     @Size(max = 20)
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "Formato de pasaporte es invalido")
     @JsonProperty("pasaporte")
-    private String pasaporte;        // RN2: Opcional
+    private String pasaporte;
 
     @NotBlank @Size(max = 45)
     @JsonProperty("nombres")
@@ -30,22 +31,25 @@ public class RegistrarVecinoCmd {
     private String apellidos;
 
     @NotBlank
-    @Pattern(regexp = "[MFmf]", message = "Género inválido (M o F)")
+    @Pattern(regexp = "[MF]", message = "Género inválido (M o F)")
     @JsonProperty("genero")
     private String genero;
 
     @NotBlank
     @Pattern(regexp = "\\d{8,15}", message = "El teléfono debe contener entre 8 y 15 dígitos")
     @JsonProperty("telefono")
-    private String telefono;         // RN2: Opcional
+    private String telefono;
 
-    @NotBlank @Email(message = "El formato del correo es inválido") @Size(max = 45)
+    @NotBlank
+    @Size(max = 45, message = "La longitud del correo sobrepasa los 45 caracteres")
+    @Email(message = "El formato del correo es inválido")
     @JsonProperty("correo")
     private String correo;
 
-    @NotBlank @Size(max = 100)
+    @NotBlank
+    @Size(max = 100)
     @JsonProperty("direccion")
-    private String direccion;        // Dirección Exacta (residencia)
+    private String direccion;
 
     @NotNull
     @Positive
@@ -57,7 +61,8 @@ public class RegistrarVecinoCmd {
     @JsonProperty("profesion_id")
     private Short profesionId;
 
-    @NotNull @Positive
+    @NotNull
+    @Positive
     @JsonProperty("locacion_id")
-    private Integer locacionId;      // Zona (as_locaciones)
+    private Integer locacionId;
 }
